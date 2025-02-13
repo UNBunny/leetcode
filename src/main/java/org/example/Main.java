890726897f721dd87917677767bd634e2298b70c
UNBunny <nikitakuharchuk07@gmail.com>
2025-02-14 01:02:59 +0600

    solved 3 easy tasks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,13 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Task_01 task_01 = new Task_01();
-        System.out.println(Arrays.toString(task_01.twoSum(new int[]{3, 3, 9, 7, 2}, 16)));
+        Task01 task01 = new Task01();
+        System.out.println(Arrays.toString(task01.twoSum(new int[]{3, 3, 9, 7, 2}, 16)));
+
+        Task09 task09 = new Task09();
+        System.out.println(task09.isPalindrome(121));
+
+        Task35 task35 = new Task35();
+        System.out.println(task35.searchInsert(new int[]{1, 3, 5, 7, 9}, 4));
     }
 }
