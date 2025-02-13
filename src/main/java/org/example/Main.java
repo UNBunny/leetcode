69063b1f70aafa1d434ea30955415c853f770b40
UNBunny <nikitakuharchuk07@gmail.com>
2025-02-14 00:29:49 +0600

    solved task01 (Two sum)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Task_01 task_01 = new Task_01();
+        System.out.println(Arrays.toString(task_01.twoSum(new int[]{3, 3, 9, 7, 2}, 16)));
     }
 }
